2b42fa4d7f76cd99576e0debc458cf97a7fbab97
amirh0ss3in <rezaeeamirhosein@gmail.com>
2022-08-20 23:00:36 +0430

    CM added

---
+++ Python/confusion_m.py
@@ -0,0 +1,53 @@
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os
+
+cwd = os.path.dirname(os.path.abspath(__file__))+'/'
+cwd = cwd.replace('\\','/')
+
+
+def create_folder(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+        print("Folder created")
+    else:
+        print("Folder already exists")
+
+def main(Normalize = True):
+    # load confusion matrixs
+    categories = ['1', '2', '3', '4', '7']
+    reports_path = cwd+"Archive/Subject wise/reports"
+    subjects = np.sort(np.array(os.listdir(reports_path), dtype = int))
+
+    save_dir = cwd+"confusion_matrixs/"
+    create_folder(save_dir)
+
+    for subject in subjects:
+        print(f"\nConfusion Matrix subject {subject}")
+        cm = np.loadtxt(reports_path+"/"+str(subject)+"/confusion_matrix.txt", dtype=int)
+        print(cm)
+
+        if Normalize:
+            cm = cm/np.sum(cm)
+
+            sns.heatmap(cm, annot=True, 
+                        fmt='.2%', cmap='Blues',
+                        xticklabels=categories,
+                        yticklabels=categories)
+        else:
+            sns.heatmap(cm, annot=True, 
+                        fmt='d', cmap='Blues',
+                        xticklabels=categories,
+                        yticklabels=categories)
+        plt.ylabel('True label')
+        plt.xlabel('Predicted label')
+        # plt.show()
+        plt.savefig(save_dir+str(subject)+".svg")
+        plt.close()
+
+
+if __name__ == "__main__":
+    main(Normalize=True)
